eda8424ecbd85d6c7e63396b711254043bae8f17
forxoops <forxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-05-13 15:07:36 +0000

    Add french language

---
+++ language/french/admin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright   The Xoops Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.gnu.org/licenses/gpl.txt GNU General Public License (GPL)
+ * @package     thadmin
+ */
+
+// General
+define('_AD_TH_PREFERENCES','Pr&eacute;f&eacute;rences');
+define('_AD_TH_UPDATE','Mise &agrave; jour');
+define('_AD_TH_TITLE', 'Administration'); 
+define('_AD_TH_NAVIGATION', 'Navigation');
+define('_AD_TH_ADMINHOME', 'Panneau de configuration');
+define('_AD_TH_SITEHOME', 'Page d\'accueil');
+define('_AD_TH_LINKS', 'Liens');
+define('_AD_TH_LOGOUT', 'D&eacute;connexion');
+define('_AD_TH_MODULES', 'Modules');
+define('_AD_TH_MODULEADMIN' , 'Administration');
+define('_AD_TH_BLOCKS', 'Blocs');
+define('_AD_TH_BLOCKADMIN', 'Administration');
+define('_AD_TH_USERS', 'Utilisateurs');
+define('_AD_TH_MISCS', 'Outils');
+
+define('_MD_AM_PREF_DESC', 'Param&egrave;tres de votre site et des modules install&eacute;s');
+define('_AD_TH_MODULES_DESC', 'G&eacute;rer les modules install&eacute;s');
+define('_AD_TH_BLOCKS_DESC', 'G&eacute;rer les blocs install&eacute;s');
+define('_AD_TH_USERS_DESC', 'G&eacute;rer les param&egrave;tres "utilisateurs"');
+define('_AD_TH_MISCS_DESC', 'Param&eacute;trer les Outils');
+define('_AD_TH_FILE', 'Pr&eacute;sence du fichier "include/cp_functions.php" :');
+?>
