25948c962c46b6227a2b1f08d339a907eaf5d75b
forxoops <forxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-05-13 15:07:36 +0000

    Add french language

---
+++ XoopsModules/thadmin/trunk/language/french/modinfo.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @copyright   The Xoops Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.gnu.org/licenses/gpl.txt GNU General Public License (GPL)
+ * @package     thadmin
+ */
+
+// Main
+define('_MI_TH_NAME', 'ThAdmin');
+define('_MI_TH_DESC', 'ThAdmin est un module pour changer le th&egrave;me de l\'administration');
+// Menu
+define('_MI_TH_HOME', 'Accueil');
+define('_MI_TH_HELP', 'Aide');
+// Settings
+define('_MI_TH_ADMINTHEME', 'Th&egrave;me Default');
+define('_MI_TH_ADMINTHEME_DESC', 'Choisissez le th&egrave;me par d&eacute;faut de votre interface d\'administration');
+?>
